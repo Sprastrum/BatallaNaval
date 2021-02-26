30b7a6bb15be363c4f806ae6a920e0f9483addc0
Santiago Santos <78430563+Sprastrum@users.noreply.github.com>
2021-02-26 14:55:10 -0500

    Delete Batalla_Naval directory

--- Batalla_Naval/src/main/java/Marine.java
+++
@@ -1,9 +0,0 @@
-public class Marine {
-    private String name;
-    private int range;
-
-    public Marine(String name, int range) {
-        this.name = name;
-        this.range = range;
-    }
-}
--- Batalla_Naval/src/main/java/Position.java
+++
@@ -1,9 +0,0 @@
-public class Position {
-    private int latitude;
-    private int longitude;
-
-    public Position(int Y, int X) {
-        this.latitude = Y;
-        this.longitude = X;
-    }
-}
